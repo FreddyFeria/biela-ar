731dd0611d8b5bcf8b63366df3409fe984fa3a2a
FreddyFeria <prometeo006@gmail.com>
2019-03-05 21:04:29 -0500

    A

--- ar-playground/events.js
+++ ar-playground/events.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent('markerhandler', {
 
     init: function() {
-        const animatedMarker = document.querySelector("#animated-marker");
+        const animatedMarker = document.querySelector("#square");
         const aEntity = document.querySelector("#animated-model");
 
         // every click, we make our model grow in size :)
